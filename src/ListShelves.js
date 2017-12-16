0290c33ef725af023fbcc7c8cf0fe3bfd27c04b6
Vlad L <vladvlaz@gmail.com>
2017-12-16 14:34:14 +1300

    Initial class creation

---
+++ src/ListShelves.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Bookshelf from './Books/Bookshelf';
+
+class ListShelves extends Component {
+
+  render() {
+    return(
+      <div>
+        <Bookshelf />
+      </div>
+    );
+  }
+}
+
+export default ListShelves;
